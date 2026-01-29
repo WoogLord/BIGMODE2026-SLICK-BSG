260dc50f9feb00f4679790f1c40e8067ed8f3d7d
WoogLord <mhnelson428@gmail.com>
2026-01-29 14:39:43 -0500

    + initart

--- code/init/initart.lua
+++ code/init/initart.lua
@@ -1,11 +1,9 @@
 function assignSpriteSheets()
     -- player
     player.spriteSheet = love.graphics.newImage("assets/art/spritesheets/player-sheet.png")
-
 end
 
 function assignLayerArt()
     -- bgs
     bg_01_dirt = love.graphics.newImage("assets/art/bgs/bg_01_dirt.png")
-
 end
